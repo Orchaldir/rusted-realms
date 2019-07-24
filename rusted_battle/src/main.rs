74031a70d4f5f8eb4d0a1e2cd40f5e30f472606d
Orchaldir <orchaldir@gmail.com>
2019-07-24 23:16:58 +0200

    [#1] Start combat test

--- rusted_battle/src/main.rs
+++ rusted_battle/src/main.rs
@@ -1,3 +1,32 @@
+#[derive(Debug)]
+struct Character {
+    fighting: i32
+}
+
+#[derive(Debug)]
+enum AttackResult {
+    Hit(i32),
+    Miss,
+}
+
+impl Character {
+
+    fn attack(self, target: Character) -> AttackResult {
+        let margin_of_success = self.fighting - target.fighting;
+
+        if margin_of_success > 0 {
+            AttackResult::Hit(margin_of_success)
+        } else {
+            AttackResult::Miss
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let character0 = Character { fighting: 6};
+    let character1 = Character { fighting: 5};
+
+    let result = character0.attack(character1);
+
+    println!("Result: {:?}", result);
 }
