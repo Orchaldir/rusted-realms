cc780d85846f9124d1d79f8354fcd70f610330f3
Orchaldir <orchaldir@gmail.com>
2019-07-25 18:01:01 +0200

    [#1] Add randomness to attack

--- rusted_battle/src/main.rs
+++ rusted_battle/src/main.rs
@@ -1,3 +1,5 @@
+extern crate rand;
+
 #[derive(Debug)]
 struct Character {
     fighting: i32,
@@ -13,7 +15,7 @@ enum AttackResult {
 impl Character {
 
     fn attack(self, target: Character) -> AttackResult {
-        let diff = self.fighting - target.fighting;
+        let diff = roll(self.fighting, target.fighting);
 
         if diff > 0 {
             let damage = diff + self.strength;
@@ -24,13 +26,26 @@ impl Character {
     }
 }
 
+fn roll(value: i32, difficulty: i32) -> i32 {
+    let sides = 10;
+    let positive_random = roll_dice(sides);
+    let negative_random = roll_dice(sides);
+    let random_modifier = positive_random - negative_random;
+    println!("Roll d{0}-d{0}={1}-{2}={3}", sides, positive_random, negative_random, random_modifier);
+    value - difficulty + random_modifier
+}
+
+fn roll_dice(sides: i32) -> i32 {
+    use rand::Rng;
+    rand::thread_rng().gen_range(1, sides + 1)
+}
+
+
 fn main() {
     let character0 = Character { fighting: 6, strength: 3 };
     let character1 = Character { fighting: 5, strength: 8 };
 
     let result = character0.attack(character1);
-
-    println!("Result: {:?}", result);
 
     match result {
         AttackResult::Hit {damage} => println!("A hit with {} damage!", damage),
