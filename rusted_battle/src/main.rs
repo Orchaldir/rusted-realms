1615c9cddd31c41cde0a3b5bcfc149ae5e4f0132
Orchaldir <orchaldir@gmail.com>
2019-07-28 09:00:31 +0200

    Use rustfmt on save

--- rusted_battle/src/main.rs
+++ rusted_battle/src/main.rs
@@ -7,24 +7,23 @@ use unit::skill::*;
 #[derive(Debug)]
 struct Character {
     fighting: i32,
-    strength: i32
+    strength: i32,
 }
 
 #[derive(Debug)]
 enum AttackResult {
-    Hit {damage: i32},
+    Hit { damage: i32 },
     Miss,
 }
 
 impl Character {
-
-    fn attack<'a, T: rand::Rng>(self, target: Character, mut checker: Checker<T>) -> AttackResult {
+    fn attack<T: rand::Rng>(self, target: Character, mut checker: Checker<T>) -> AttackResult {
         match checker.check(self.fighting, target.fighting) {
-            CheckResult::Success {degree} => {
+            CheckResult::Success { degree } => {
                 let damage = self.strength * degree as i32;
-                AttackResult::Hit {damage }
-            },
-            _ => AttackResult::Miss
+                AttackResult::Hit { damage }
+            }
+            _ => AttackResult::Miss,
         }
     }
 }
@@ -32,13 +31,19 @@ impl Character {
 fn main() {
     let mut rng = rand::thread_rng();
     let checker = unit::skill::build_checker(10, 5, &mut rng);
-    let character0 = Character { fighting: 6, strength: 3 };
-    let character1 = Character { fighting: 5, strength: 8 };
+    let character0 = Character {
+        fighting: 6,
+        strength: 3,
+    };
+    let character1 = Character {
+        fighting: 5,
+        strength: 8,
+    };
 
     let result = character0.attack(character1, checker);
 
     match result {
-        AttackResult::Hit {damage} => println!("A hit with {} damage!", damage),
-        AttackResult::Miss => println!("A miss :(")
+        AttackResult::Hit { damage } => println!("A hit with {} damage!", damage),
+        AttackResult::Miss => println!("A miss :("),
     }
 }
--- rusted_battle/src/unit/skill.rs
+++ rusted_battle/src/unit/skill.rs
@@ -4,31 +4,36 @@ extern crate rand;
 pub struct Checker<'a, T: rand::Rng> {
     die_sides: u32,
     size_of_degree: u32,
-    rng: & 'a mut T,
+    rng: &'a mut T,
 }
 
 #[derive(Debug)]
 pub enum CheckResult {
     Success { degree: u32 },
-    Failure { degree: u32 }
+    Failure { degree: u32 },
 }
 
-impl<'a, T: rand::Rng> Checker<'a, T> {
-
+impl<'a, T> Checker<'a, T>
+where
+    T: rand::Rng,
+{
     pub fn check(&mut self, skill: i32, difficulty: i32) -> CheckResult {
         let positive_die = self.roll_die();
         let negative_die = self.roll_die();
         let random_modifier = positive_die - negative_die;
-        println!("Roll d{0}-d{0}={1}-{2}={3}", self.die_sides, positive_die, negative_die, random_modifier);
+        println!(
+            "Roll d{0}-d{0}={1}-{2}={3}",
+            self.die_sides, positive_die, negative_die, random_modifier
+        );
+
         let diff = skill - difficulty + random_modifier;
         let degree = 1 + diff.abs() as u32 / self.size_of_degree;
-        println!("diff={} degree={}",diff, degree);
+        println!("diff={} degree={}", diff, degree);
 
         if diff >= 0 {
-            CheckResult::Success {degree}
-        }
-        else {
-            CheckResult::Failure {degree}
+            CheckResult::Success { degree }
+        } else {
+            CheckResult::Failure { degree }
         }
     }
 
@@ -37,6 +42,13 @@ impl<'a, T: rand::Rng> Checker<'a, T> {
     }
 }
 
-pub fn build_checker<T: rand::Rng>(die_sides: u32, size_of_degree: u32, rng: & mut T) -> Checker<T> {
-    Checker { die_sides, size_of_degree, rng }
+pub fn build_checker<T>(die_sides: u32, size_of_degree: u32, rng: &mut T) -> Checker<T>
+where
+    T: rand::Rng,
+{
+    Checker {
+        die_sides,
+        size_of_degree,
+        rng,
+    }
 }
