31fa1bb55dd177c87ec40b97d9b1aa7b86cc96cd
Orchaldir <orchaldir@gmail.com>
2019-08-16 09:33:13 +0200

    [#9] Add Unit.get_skill_rank()

--- rusted_battle/src/unit/mod.rs
+++ rusted_battle/src/unit/mod.rs
@@ -1,13 +1,15 @@
+use crate::unit::condition::set::ConditionSet;
 use crate::unit::skill::set::SkillSet;
+use crate::unit::skill::Skill;
 
 pub mod condition;
 pub mod health;
 pub mod skill;
 
-#[derive(Debug)]
 pub struct Unit<'a> {
     name: &'a str,
     skill_set: SkillSet<'a>,
+    conditions: ConditionSet<'a>,
 }
 
 impl<'a> Unit<'a> {
@@ -15,6 +17,33 @@ impl<'a> Unit<'a> {
         Unit {
             name,
             skill_set: SkillSet::new(),
+            conditions: ConditionSet::new(),
         }
+    }
+
+    pub fn get_skill_rank(&self, skill: &'a Skill<'a>) -> i32 {
+        let rank = self.skill_set.get_skill_rank(skill);
+        let modifier = self.conditions.get_skill_modifier(skill);
+
+        rank + modifier
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::unit::condition::modifier::Modifier;
+    use crate::unit::condition::skill_modifier::SkillModifier;
+
+    #[test]
+    fn test_get_skill_rank() {
+        let skill_a = Skill::new("a");
+        let condition_a = SkillModifier::new("cond_a", &skill_a, 2);
+        let mut unit = Unit::new("test");
+
+        unit.conditions.add(&condition_a);
+        unit.skill_set.set_skill_rank(&skill_a, 3);
+
+        assert_eq!(unit.get_skill_rank(&skill_a), 5)
     }
 }
